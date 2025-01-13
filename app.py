82a1472436eff2ebda0c266b71453179d89581f5
SarahGMH <98407518+SarahGMH@users.noreply.github.com>
2025-01-13 14:16:44 +0100

    Update app.py

--- app.py
+++ app.py
@@ -9,3 +9,5 @@ matrice = np.array([[12, 45, 78,89], [56,78, 98, 123], [34, 56, 78,89],[45,57,89
 x_min = np.min(matrice)
 x_max = np.max(matrice)
 normalized_matrice =  (matrice - x_min ) / (x_max - x_min)
+
+print(normalized_matrice)
