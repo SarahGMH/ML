ea9681bb4e00161420176d6d2e1249964652a1d0
SarahGMH <98407518+SarahGMH@users.noreply.github.com>
2025-01-13 14:05:25 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+#Matrice initiale 
+
+matrice = np.array([[12, 45, 78,89], [56,78, 98, 123], [34, 56, 78,89],[45,57,89,100] , [23,45,67,78],[78,98,123,145],[56,78,98,123],[34,56,78,89]])   
+
+
+
+x_min = np.min(matrice)
+x_max = np.max(matrice)
+normalized_matrice =  (matrice - x_min ) / (x_max - x_min)
