182db80ffd21a0d7e0c0fe1a09b21915e757d446
SarahGMH <98407518+SarahGMH@users.noreply.github.com>
2025-01-14 16:54:58 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,31 @@
-import numpy as np
+import cv2 
+import face_recognition
+# Charger une image 
+image = cv2.imread('me.jpg') 
+# Afficher l'image 
+cv2.imshow('Image', image) 
+cv2.waitKey(0) 
+cv2.destroyAllWindows()
 
-#Matrice initiale 
+# Convertir en niveaux de gris 
+gray_image = cv2.cvtColor(image, 
+cv2.COLOR_BGR2GRAY) 
+# Sauvegarder ou afficher 
+cv2.imshow('Grayscale Image', gray_image) 
+cv2.imwrite('gray_image.jpg', gray_image)
 
-matrice = np.array([[12, 45, 78,89], [56,78, 98, 123], [34, 56, 78,89],[45,57,89,100] , [23,45,67,78],[78,98,123,145],[56,78,98,123],[34,56,78,89]])   
+# Normalisation entre 0 et 1 
+normalized_image = image / 255.0 
+# Normalisation à une plage personnalisée 
+norm_image = cv2.normalize(image, None, 
+alpha=0, beta=255, norm_type=cv2.NORM_MINMAX)
 
 
 
-x_min = np.min(matrice)
-x_max = np.max(matrice)
-normalized_matrice =  (matrice - x_min ) / (x_max - x_min)
-
-print(normalized_matrice)
+nown_image = face_recognition.load_image_file('file1.jpg')
+nknown_image = face_recognition.load_image_file('file2.jpg')
+biden_encoding = face_recognition.face_encodings(known_image)[0]
+unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
+results = face_recognition.compare_faces([biden_encoding], 
+unknown_encoding)
+print(str(results))
